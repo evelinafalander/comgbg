cb57692e54c1f501e4310974611bcd6e06ee2d17
evelinafalander <evel0838@stud.kea.dk>
2023-02-02 21:14:09 +0100

    last fixes

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+//  SCROLL IN OPACITY ANIMATION
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 30;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
